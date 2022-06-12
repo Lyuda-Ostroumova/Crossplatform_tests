30ffd539e54a3f5034856ae0346cc5813dd9e2b3
Lyudmila Ostroumova <lyudmila.ostroumova@gmail.com>
2022-06-12 19:40:02 +0300

    Elements added

---
+++ src/main/java/ru/netology/pages/Elements.java
@@ -0,0 +1,35 @@
+package ru.netology.pages;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.support.PageFactory;
+
+import java.time.Duration;
+
+public class Elements {
+
+    @AndroidFindBy(id = "ru.netology.testing.uiautomator:id/userInput")
+    public MobileElement textInput;
+
+    @AndroidFindBy(id = "ru.netology.testing.uiautomator:id/buttonChange")
+    public MobileElement changeTextButton;
+
+    @AndroidFindBy(id = "textToBeChanged")
+    public MobileElement newText;
+
+    @AndroidFindBy(id = "ru.netology.testing.uiautomator:id/buttonActivity")
+    public MobileElement newActivityBtn;
+
+    @AndroidFindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.widget.TextView")
+    public MobileElement newActivityText;
+
+    private AppiumDriver driver;
+
+    public Elements(AppiumDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(new AppiumFieldDecorator(driver, Duration.ofSeconds(15)), this);
+    }
+
+}
--- src/test/java/ru/netology/test/TextTest.java
+++ src/test/java/ru/netology/test/TextTest.java
@@ -1,11 +1,11 @@
 package ru.netology.test;
 
-import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import java.net.MalformedURLException;
 import java.net.URL;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import ru.netology.pages.Elements;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class TextTest {
@@ -30,33 +30,21 @@ public class TextTest {
 
     @Test
     public void emptyTextTest() {
-        MobileElement el3 = (MobileElement) driver.findElementById("ru.netology.testing.uiautomator:id/userInput");
-        el3.click();
-        el3.sendKeys("Hello!");
-        MobileElement el4 = (MobileElement) driver.findElementById("ru.netology.testing.uiautomator:id/buttonChange");
-        el4.click();
-        MobileElement e19 = (MobileElement) driver.findElementById("textToBeChanged");
-        Assertions.assertEquals("Hello!", e19.getText());
-
-        MobileElement el5 = (MobileElement) driver.findElementById("ru.netology.testing.uiautomator:id/userInput");
-        el5.click();
-        el5.sendKeys(" ");
-        MobileElement el6 = (MobileElement) driver.findElementById("ru.netology.testing.uiautomator:id/buttonChange");
-        el6.click();
-        MobileElement e20 = (MobileElement) driver.findElementById("textToBeChanged");
-        Assertions.assertEquals(e19.getText(), e20.getText());
-
+        Elements elements = new Elements(driver);
+        elements.textInput.sendKeys("Hello!");
+        elements.changeTextButton.click();
+        Assertions.assertEquals("Hello!", elements.newText.getText());
+        elements.textInput.sendKeys(" ");
+        elements.changeTextButton.click();
+        Assertions.assertEquals("Hello!", elements.newText.getText());
     }
 
     @Test
     public void newActivityTest() {
-        MobileElement el7 = (MobileElement) driver.findElementById("ru.netology.testing.uiautomator:id/userInput");
-        el7.click();
-        el7.sendKeys("Good Afternoon!");
-        MobileElement el8 = (MobileElement) driver.findElementById("ru.netology.testing.uiautomator:id/buttonActivity");
-        el8.click();
-        MobileElement e21 = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.widget.TextView");
-        Assertions.assertEquals("Good Afternoon!", e21.getText());
+        Elements elements = new Elements(driver);
+        elements.textInput.sendKeys("Good Afternoon!");
+        elements.newActivityBtn.click();
+        Assertions.assertEquals("Good Afternoon!", elements.newActivityText.getText());
     }
 
     @AfterAll
